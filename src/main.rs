ad54aa09a4691d00ca2ef1e4eef7ec5dae40e0e9
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2023-12-05 22:27:53 +0000

    task 4

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 mod task01;
 mod task02;
 mod task03;
-// mod task04;
+mod task04;
 // mod task05;
 // mod task06;
 // mod task07;
@@ -18,8 +18,8 @@ mod task03;
 
 // use task01::task01;
 // use task02::task02;
-use task03::task03;
-// use task04::task04;
+// use task03::task03;
+use task04::task04;
 // use task05::task05;
 // use task06::task06;
 // use task07::task07;
@@ -31,5 +31,5 @@ use task03::task03;
 // use task13::task13;
 
 fn main() {
-    task03();
+    task04();
 }
