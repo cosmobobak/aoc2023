032d9dbd21e1919e0177fb5ce433fdd5dde669b3
cosmobobak <cosmobobak@gmail.com>
2023-12-16 14:51:32 +0000

    parallelism!

--- src/task05.rs
+++ src/task05.rs
@@ -4,16 +4,13 @@
     clippy::cast_possible_wrap
 )]
 
+use rayon::slice::ParallelSlice;
+use rayon::prelude::*;
+
 #[derive(Debug, Clone, Copy)]
 struct Mapping {
     dst_start: u64,
     src_start: u64,
-    len: u64,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct ValueRange {
-    start: u64,
     len: u64,
 }
 
@@ -24,127 +21,24 @@ impl Mapping {
         }
         Some(value + self.dst_start - self.src_start)
     }
-
-    const fn map_vr(&self, vr: ValueRange) -> Option<(ValueRange, Option<ValueRange>, Option<ValueRange>)> {
-        if vr.start + vr.len <= self.src_start || vr.start >= self.src_start + self.len {
-            return None;
-        }
-
-        // if the value range is completely inside of the mapping, return the mapped value range
-        if vr.start >= self.src_start && vr.start + vr.len <= self.src_start + self.len {
-            let moved = ValueRange {
-                start: vr.start + self.dst_start - self.src_start,
-                len: vr.len,
-            };
-            return Some((moved, None, None));
-        }
-
-        // if the mapping is completely inside the value range, return the mapped value range and both halves of the leftovers
-        if self.src_start >= vr.start && self.src_start + self.len <= vr.start + vr.len {
-            let moved = ValueRange {
-                start: self.dst_start,
-                len: self.len,
-            };
-            let unmoved_left = ValueRange {
-                start: vr.start,
-                len: self.src_start - vr.start,
-            };
-            let unmoved_right = ValueRange {
-                start: self.src_start + self.len,
-                len: vr.len - moved.len - unmoved_left.len,
-            };
-            return Some((moved, Some(unmoved_left), Some(unmoved_right)))
-        }
-
-        // left overlap:
-        if vr.start < self.src_start {
-            // truncate this range to that which remains unmoved
-            let unmoved = ValueRange {
-                start: vr.start,
-                len: self.src_start - vr.start,
-            };
-            // create the moved section
-            let moved = ValueRange {
-                start: self.dst_start,
-                len: vr.len - unmoved.len,
-            };
-            return Some((moved, Some(unmoved), None));
-        }
-
-        // right overlap:
-        let moved = ValueRange {
-            start: vr.start + self.dst_start - self.src_start,
-            len: (self.src_start + self.len) - vr.start,
-        };
-        let unmoved = ValueRange {
-            start: self.src_start + self.len,
-            len: vr.len - moved.len,
-        };
-
-        Some((moved, Some(unmoved), None))
-    }
 }
 
-fn map_any(vr: ValueRange, mappings: &[Mapping]) -> Vec<ValueRange> {
-    let mut source = vec![vr];
-    let mut results = Vec::new();
-    for &mapping in mappings {
-        'scan: loop {
-            for i in 0..source.len() {
-                if let Some((m, m2, m3)) = mapping.map_vr(source[i]) {
-                    results.push(m);
-                    if let Some(m2) = m2 {
-                        source[i] = m2;
+fn compute_lowest_mapping(seeds: impl ParallelIterator<Item = u64>, layers: &[&[Mapping]]) -> u64 {
+    seeds
+        .map(|s| {
+            let mut s = s;
+            for &layer in layers {
+                for &mapping in layer {
+                    if let Some(mapped) = mapping.map(s) {
+                        s = mapped;
+                        break;
                     }
-                    if let Some(m3) = m3 {
-                        source.push(m3);
-                    }
-                    continue 'scan;
                 }
             }
-            break 'scan;
-        }
-    }
-    if results.is_empty() {
-        results.push(vr);
-    }
-    results
-}
-
-fn compute_lowest_mapping(seeds: &[ValueRange], layers: &[Vec<Mapping>]) -> u64 {
-    let n_seeds = seeds.len();
-    let res = seeds
-        .iter()
-        .enumerate()
-        .flat_map(|(i, s)| {
-            let mut ranges = vec![*s];
-            let mut new_ranges = Vec::new();
-            for layer in layers {
-                for &vr in &ranges {
-                    let resulting_ranges = map_any(vr, layer);
-                    println!(
-                        "ValueRange [{}-{}] with layer\n{:?}\nmaps to [{}]",
-                        vr.start,
-                        vr.start + vr.len,
-                        layer,
-                        resulting_ranges.iter().map(|vr| format!("[{}-{}]", vr.start, vr.start + vr.len)).collect::<Vec<_>>().join(",")
-                    );
-                    new_ranges.extend_from_slice(&resulting_ranges);
-                }
-                ranges.clear();
-                std::mem::swap(&mut ranges, &mut new_ranges);
-            }
-            // report progress
-            print!("\r{}/{}", i + 1, n_seeds);
-            // flush stdout
-            std::io::Write::flush(&mut std::io::stdout()).unwrap();
-            ranges
+            s
         })
-        .map(|vr| vr.start)
         .min()
-        .unwrap();
-    println!();
-    res
+        .unwrap()
 }
 
 pub fn task05() {
@@ -153,13 +47,14 @@ pub fn task05() {
 
     let (seeds, layers) = task.split_once("\n\n").unwrap();
 
-    let seed_ints = seeds
+    let seeds = seeds
         .split_once(": ")
         .unwrap()
         .1
         .split_whitespace()
         .map(|s| s.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
+
     let layers = layers
         .split("\n\n")
         .map(|layer| {
@@ -181,24 +76,29 @@ pub fn task05() {
         })
         .collect::<Vec<_>>();
 
-    let single_seeds = seed_ints
+    let lengths = layers.iter().map(Vec::len).collect::<Vec<_>>();
+    let layers_buffer = layers.into_iter().flatten().collect::<Vec<_>>();
+    let layers = lengths
         .iter()
-        .map(|&s| ValueRange { start: s, len: 1 })
-        .collect::<Vec<_>>();
-
-    let lowest_mapping = compute_lowest_mapping(&single_seeds, &layers);
-
-    println!("Part 1: {lowest_mapping}");
-
-    let seed_ranges = seed_ints
-        .chunks(2)
-        .map(|c| ValueRange {
-            start: c[0],
-            len: c[1],
+        .scan(0, |offset, &len| {
+            let res = &layers_buffer[*offset..*offset + len];
+            *offset += len;
+            Some(res)
         })
         .collect::<Vec<_>>();
 
-    let lowest_mapping = compute_lowest_mapping(&seed_ranges, &layers);
+    let lowest_mapping = compute_lowest_mapping(seeds.par_iter().copied(), &layers);
+
+    println!("Part 1: {lowest_mapping}");
+
+    let range_mapping = seeds
+        .par_chunks(2)
+        .flat_map(|chunk| {
+            let start = chunk[0];
+            let count = chunk[1];
+            start..start + count
+        });
+    let lowest_mapping = compute_lowest_mapping(range_mapping, &layers);
 
     println!("Part 2: {lowest_mapping}");
     let elapsed = start.elapsed();
